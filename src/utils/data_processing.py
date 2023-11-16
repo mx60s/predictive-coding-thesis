2545dbb73bde5d3fb01154a53c397e61d7b837c1
Maggie von Ebers <vonebersm@gmail.com>
2023-11-15 19:43:25 -0600

    little tool to fix some of my data

--- src/utils/data_processing.py
+++ src/utils/data_processing.py
@@ -1,16 +1,39 @@
 import numpy as np
 
 def remove_consecutive_repeats(np_file_path):
-    arrays = np.load(np_file_path, allow_pickle=True)
-    unique_arrays = []
-    print(len(arrays))
-    for i in range(1, len(arrays) - 1):
-        if not np.array_equal(arrays[i], arrays[i + 1]):
-            unique_arrays.append(arrays[i])
+    """
+    Returns an array of indices with the consecutive repeating frames removed. To be
+    used on coordinate files and then applied to the frame files.
+    """
+    unique_idx = []
+    data = np.load(np_file_path, allow_pickle=True)
+    for i in range(len(data) - 1):
+        if not np.array_equal(data[i], data[i + 1]):
+            unique_idx.append(i)
 
-    if len(arrays) > 0:
-        unique_arrays.append(arrays[-1])
+    if len(data) > 0:
+        unique_idx.append(len(data) - 1)
 
-    print(len(unique_arrays))
-    return unique_arrays
-    #np.save(np_file_path, unique_arrays)
+    del data
+    return unique_idx
+    
+    
+def map_files_to_chunks(source_directory, target_directory, file_start, seq_len):
+    print('Indexing files to', target_directory)
+    os.makedirs(target_directory)
+
+    file_index = 0
+    for filename in os.listdir(source_directory):
+        if filename.startswith(file_start):
+            filepath = os.path.join(source_directory, filename)
+            data = np.load(filepath, mmap_mode='r')
+    
+            for i in range(len(data) - (seq_len + 1)):
+                chunk = data[i:i + seq_len + 1]
+                chunk_fp = os.path.join(target_directory, f'{file_index}.npy')
+                np.save(chunk_fp, chunk)
+                file_index += 1
+    
+            del data
+
+    return file_index
--- src/utils/model_tools.py
+++ src/utils/model_tools.py
@@ -3,28 +3,9 @@ import torchmetrics
 import numpy as np
 from torch.utils.data import Dataset
 import os
+from utils.data_processing import map_files_to_chunks
 
 # thank you Marlan
-
-def map_files_to_chunks(source_directory, target_directory, file_start, seq_len):
-    print('Indexing files to', target_directory)
-    os.makedirs(target_directory)
-
-    file_index = 0
-    for filename in os.listdir(source_directory):
-        if filename.startswith(file_start):
-            filepath = os.path.join(source_directory, filename)
-            data = np.load(filepath, mmap_mode='r')
-    
-            for i in range(len(data) - (seq_len + 1)):
-                chunk = data[i:i + seq_len + 1]
-                chunk_fp = os.path.join(target_directory, f'{file_index}.npy')
-                np.save(chunk_fp, chunk)
-                file_index += 1
-    
-            del data
-
-    return file_index
 
 class SequentialFrameDataset(Dataset):
     """
